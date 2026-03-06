8821d1c750dd1578f0b10bf553c7aa2fb67326d1
Son Roy Almerol <sralmerol@sgl.com>
2026-03-06 13:33:29 -0500

    box64 script: standardize spacings + consistent fallback for BASH_PATH

--- box64.sh
+++ box64.sh
@@ -1,48 +1,51 @@
 #!/bin/bash
 
-if [[ -z "$ARM64_DEVICE" ]]; then
-    ARM64_DEVICE="generic"
-fi
+ARM64_DEVICE="${ARM64_DEVICE:-generic}"
 
 case "$ARM64_DEVICE" in
-    rpi5)      SUFFIX="rpi5" ;;
-    rpi5_16k)  SUFFIX="rpi5-16k" ;;
-    rpi4)      SUFFIX="rpi4" ;;
-    rpi3)      SUFFIX="rpi3" ;;
-    rk3588)    SUFFIX="rk3588" ;;
-    rk3399)    SUFFIX="rk3399" ;;
-    m1|apple)  SUFFIX="m1" ;;
-    tegrax1)   SUFFIX="tegrax1" ;;
-    tegra_t194) SUFFIX="tegra-t194" ;;
-    android)   SUFFIX="android" ;;
-    lx2160a)   SUFFIX="lx2160a" ;;
-    sd888)     SUFFIX="sd888" ;;
-    sdoryon1)  SUFFIX="sdoryon1" ;;
-    *)         SUFFIX="generic" ;;
+  rpi5) SUFFIX="rpi5" ;;
+  rpi5_16k) SUFFIX="rpi5-16k" ;;
+  rpi4) SUFFIX="rpi4" ;;
+  rpi3) SUFFIX="rpi3" ;;
+  rk3588) SUFFIX="rk3588" ;;
+  rk3399) SUFFIX="rk3399" ;;
+  m1 | apple) SUFFIX="m1" ;;
+  tegrax1) SUFFIX="tegrax1" ;;
+  tegra_t194) SUFFIX="tegra-t194" ;;
+  android) SUFFIX="android" ;;
+  lx2160a) SUFFIX="lx2160a" ;;
+  sd888) SUFFIX="sd888" ;;
+  sdoryon1) SUFFIX="sdoryon1" ;;
+  *) SUFFIX="generic" ;;
 esac
 
-export BOX64_BASH="/usr/local/bin/box64-bash-$SUFFIX"
 BINARY_PATH="/usr/local/bin/box64-$SUFFIX"
+BASH_PATH="/usr/local/bin/box64-bash-$SUFFIX"
 
-# Fallback to generic if the specific one is missing
 if [[ ! -x "$BINARY_PATH" ]]; then
-    BINARY_PATH="/usr/local/bin/box64-generic"
+  BINARY_PATH="/usr/local/bin/box64-generic"
+  BASH_PATH="/usr/local/bin/box64-bash-generic"
 fi
 
-# SteamCMD crash masking logic
+export BOX64_BASH="$BASH_PATH"
+
 IS_STEAMCMD=0
 for arg in "$@"; do
-    if [[ "$arg" == *"steamcmd"* ]]; then
-        IS_STEAMCMD=1
-        break
-    fi
+  if [[ "$arg" == *"steamcmd"* ]]; then
+    IS_STEAMCMD=1
+    break
+  fi
 done
 
-if [ "$IS_STEAMCMD" -eq 1 ]; then
-    "$BINARY_PATH" "$@"
-    STATUS=$?
-    [[ $STATUS -eq 139 || $STATUS -eq 134 ]] && exit 0
-    exit $STATUS
+if [[ "$IS_STEAMCMD" -eq 1 ]]; then
+  export BOX64_SHOWSEGV=1
+  export BOX64_DLSYM_ERROR=1
+  "$BINARY_PATH" "$@"
+  STATUS=$?
+  if [[ $STATUS -eq 139 || $STATUS -eq 134 ]]; then
+    exit 0
+  fi
+  exit "$STATUS"
 else
-    exec "$BINARY_PATH" "$@"
+  exec "$BINARY_PATH" "$@"
 fi
